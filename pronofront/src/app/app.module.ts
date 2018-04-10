ab97462b83f8347b9b69c7521dc5e4a4dfe099fc
antoine <bergamoantoine@gmail.com>
2018-04-10 16:53:23 +0200

    front

--- pronofront/src/app/app.module.ts
+++ pronofront/src/app/app.module.ts
@@ -5,13 +5,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ConnectionComponent } from './connection/connection.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ClassementComponent } from './classement/classement.component';
+import { ProfilComponent } from './profil/profil.component';
+import { MatchComponent } from './match/match.component';
+import { ListesComponent } from './listes/listes.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ConnectionComponent,
-    InscriptionComponent
+    InscriptionComponent,
+    ClassementComponent,
+    ProfilComponent,
+    MatchComponent,
+    ListesComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
--- pronofront/src/app/connection/connection.component.ts
+++ pronofront/src/app/connection/connection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-connection',
   templateUrl: './connection.component.html',
