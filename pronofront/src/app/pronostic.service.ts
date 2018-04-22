5bb6a3e7a54356f879d85eec86cf291e19d4cc21
mranchon <dm.ranchon@gmail.com>
2018-04-22 14:12:23 +0200

    presentation ready

--- pronofront/src/app/pronostic.service.ts
+++ pronofront/src/app/pronostic.service.ts
@@ -8,11 +8,32 @@ const HttpOptions = {
   headers: new HttpHeaders({'Content-type' : 'application/json'})
 };
 
+const baseURL= HttpClientHelper.baseURL;
+
 @Injectable()
 export class PronosticService {
   constructor(private http: HttpClient) {}
 
   getPronostics() {
-    return this.http.get(`${HttpClientHelper.baseURL}/api/pronostics`);
+    return this.http.get(`${baseURL}/api/pronostics`);
+  }
+
+  getPronostic(id) {
+    return this.http.get(`${baseURL}/api/pronostics/` + id);
+  }
+
+  createPronostic(pronostic) {
+    let body = JSON.stringify(pronostic);
+    return this.http.post(`${baseURL}/api/pronostics`, body, HttpOptions);
+  }
+
+  updatePronostic(pronostic) {
+    let body = JSON.stringify(pronostic);
+    return this.http.put(`${baseURL}/api/pronostics/` + pronostic.id, body, HttpOptions);
+
+  }
+
+  deletePronostic(pronostic) {
+    return this.http.delete(`${baseURL}/api/pronostics/` + pronostic.id);
   }
 }
