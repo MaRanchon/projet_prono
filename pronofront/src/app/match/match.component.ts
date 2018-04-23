8e039cc5846ffb4b15a1a4f7950107b69143817b
mranchon <dm.ranchon@gmail.com>
2018-04-23 15:35:34 +0200

    prono

--- pronofront/src/app/match/match.component.ts
+++ pronofront/src/app/match/match.component.ts
@@ -22,7 +22,24 @@ export class MatchComponent implements OnInit {
   augm2(){
     this.count2+=1;
   }
-  
+
+  createPronostic(scoreEquipe1, scoreEquipe2) {
+    let pronostic = {
+      idMatch : 1,
+      scoreEquipe1 : scoreEquipe1,
+      scoreEquipe2 : scoreEquipe2
+    };
+    this.pronosticService.createPronostic(pronostic).subscribe(
+      data => {
+        console.log('Requête envoyée');
+        return true;
+      },
+      err => {
+        console.log("une erreur est survenue");
+        return Observable.throw(err);
+      }
+    );
+  }
 
 
 }
